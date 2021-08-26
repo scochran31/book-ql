865ad2d6a0e0d5bffe070c4697e5fc3d1592fc22
Shawn Cochran <shawn.cochran31@gmail.com>
2021-08-25 19:21:02 -0600

    mutations done, queries ok

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,20 +1,65 @@
-import gql from '@apollo/client';
+import { gql } from '@apollo/client';
 
-export const GET_ME = gql`
-{
-    me{
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            title
-            description
-            link
-            image
+const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password) {
+            token
+            user{
+                _id
+                username
+            }
         }
     }
-}
-`
+`;
+
+const ADD_USER = gql`
+    mutation addUser($email: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!){
+        saveBook(bookData: $bookData){
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                authors
+                title
+                description
+                link
+                image
+            }
+        }
+    }
+`;
+
+const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                authors
+                title
+                description
+                link
+                image
+        }
+    }
+`;
+
+export default { ADD_USER, LOGIN_USER, SAVE_BOOK, REMOVE_BOOK };
--- server/Schema/resolvers.js
+++ server/Schema/resolvers.js
@@ -5,69 +5,63 @@ const db = require('../config/connection')
 
 const resolvers = {
     Query: {
-        // me: async (parent, args, context) => {
-        //     if (context.user) {
-        //         const userData = await User.findOne({ _id: context.user._id })
-        //             .select('-__v -password')
-        //             .populate('savedBooks');
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks');
 
-        //         return userData;
-        //     }
-        //     throw new AuthenticationError('Not logged in');
-        // }
-        me: async () => {
-            const results = await db.models.User.find();
-            console.log(results);
-
-            return results;
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
         }
     },
 
-    // Mutation: {
-    //     addUser: async (parent, args) => {
-    //         const user = await User.create(args);
-    //         const token = signToken(user);
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
-    //         return { token, user };
-    //     },
-    //     login: async (parent, { email, password }) => {
-    //         const user = await User.findOne({ email });
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
-    //         if (!user) {
-    //             throw new AuthenticationError('Wrong email');
-    //         }
-    //         const correctPw = await user.isCorrectPassword(password);
+            if (!user) {
+                throw new AuthenticationError('Wrong email');
+            }
+            const correctPw = await user.isCorrectPassword(password);
 
-    //         if (!correctPw) {
-    //             throw new AuthenticationError('Wrong password!');
-    //         }
-    //         const token = signToken(user);
-    //         return { token, user };
-    //     },
-    //     saveBook: async (parent, { bookData }, context) => {
-    //         if (context.book) {
-    //             const updatedBook = await Book.findByIdAndUpdate(
-    //                 { _id: context.book._id },
-    //                 { $push: { saveBook: bookData } },
-    //                 { new: true }
-    //             );
-    //             return updatedBook;
-    //         }
-    //         throw new AuthenticationError('You need to be logged in!')
-    //     },
-    //     removeBook: async (parent, { bookId }, context) => {
-    //         if (context.book) {
-    //             const updatedBook = await Book.findByIdAndUpdate(
-    //                 { _id: context.book._id },
-    //                 { $pull: { savedBooks: bookId } },
-    //                 { new: true }
-    //             ).populate('savedBooks');
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.book) {
+                const updatedBook = await Book.findByIdAndUpdate(
+                    { _id: context.book._id },
+                    { $push: { saveBook: bookData } },
+                    { new: true }
+                );
+                return updatedBook;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.book) {
+                const updatedBook = await Book.findByIdAndUpdate(
+                    { _id: context.book._id },
+                    { $pull: { savedBooks: bookId } },
+                    { new: true }
+                ).populate('savedBooks');
 
-    //             return updatedBook;
-    //         }
-    //         throw new AuthenticationError('You need to be logged in!')
-    //     }
-    // }
+                return updatedBook;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        }
+    }
 };
 
 module.exports = resolvers;
